d101db06737dc7c953569f11bf9c8ef518c9288f
Your Name <egwujennifer@yahoo.com>
2022-06-14 22:26:17 +0100

    even and odd

---
+++ even_and_odd.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 14 21:08:11 2022
+
+@author: DELL
+"""
+#Determining an even number using if statements.
+
+num = int(input('Pleses enter a number: '))
+if num % 2 == 0:
+    print(num, "is an even number")
+else:
+    print(num, 'is not an even number')
+    
+    
+    
+#determining an odd number
+num = int(input('Please enter a number: '))
+if num % 2 != 0:
+    print(num, 'is an odd number.')
+else:
+    print(num, 'is not an odd number')
+    
